d224d0f18046856c8297121fb879b39c8736d326
Chase Clark <chase_clark07@hotmail.com>
2021-02-07 21:50:22 -0500

    Initial commit

---
+++ src/edu/frostburg/cosc444/ClarkChaseMultithreading/Main.java
@@ -0,0 +1,11 @@
+package edu.frostburg.cosc444.ClarkChaseMultithreading;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // maybe make an extension of the thread class
+        System.out.println("test");
+
+    }
+}
