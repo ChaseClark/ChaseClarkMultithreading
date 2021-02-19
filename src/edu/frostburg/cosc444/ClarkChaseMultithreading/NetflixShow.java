b3cd2b3928abfd4093182e657755ae55ebc3ef36
Chase J. Clark <chase_clark07@hotmail.com>
2021-02-19 09:33:57 -0500

    test

---
+++ src/edu/frostburg/cosc444/ClarkChaseMultithreading/NetflixShow.java
@@ -0,0 +1,17 @@
+package edu.frostburg.cosc444.ClarkChaseMultithreading;
+
+/*
+* Model class to hold information about a Netflix show
+* */
+public class NetflixShow {
+    private String _id;
+    private String _type;
+    private String _title;
+    private String _director;
+    private String _dateAdded;
+    private String _releaseYear;
+    private String _rating;
+    private String _duration;
+    private boolean _isLocked = false; // default to unlocked
+
+}
